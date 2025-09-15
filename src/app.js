022bd408c1b58ca254e5230d5bffb93cf1c9883f
syedasadhashmi <asadhashmi22@gmail.com>
2025-09-15 12:46:41 +0500

    get api's

--- src/app.js
+++ src/app.js
@@ -8,22 +8,67 @@ const app = express();
 
 const User = require("./models/user");
 
+app.use(express.json());
+
+// SignUp
 app.post("/signup", async (req, res) => {
   // creating a new instance of user Model
-  const user = new User({
-    firstName: "Syed Asad",
-    lastName: "Hashmi",
-    email: "asadhashmi@gmai.com",
-    password: "asad@123",
-    gender: "male",
-    age: 28,
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
     res.send("user created succesfully");
   } catch (err) {
     res.status(400).send("Error saving the user ", err.message);
+  }
+});
+
+// Get user By Email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
+  console.log(userEmail);
+  try {
+    //It fetches the oldest one
+    const users = await User.findOne({
+      email: userEmail,
+    });
+    if (!users) {
+      res.status(404).send("Record Not Found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// Get all users for feed
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (!users) {
+      res.status(404).send("Record Not Found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// Get By ID
+app.get("/findById", async (req, res) => {
+  const userId = req.body._id;
+
+  try {
+    const users = await User.findById(userId);
+    if (!users) {
+      res.status(404).send("Record Not Found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
   }
 });
 
@@ -35,5 +80,5 @@ connectDb()
     });
   })
   .catch(() => {
-    console.log("Data base cannot be connexted");
+    console.log("Data base cannot be connected");
   });
